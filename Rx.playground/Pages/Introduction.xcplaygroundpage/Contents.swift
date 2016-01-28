f545ef8f1d1f1eafd50cd95cb06848f6d0187961
Mike Finney <finneycanhelp@gmail.com>
2016-01-27 20:16:57 -0600

    Changed Basics to Concepts

--- Rx.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Rx.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ import Foundation
 A vast majority of the code we write revolves around responding to external actions. When a user manipulates a control, we need to write an @IBAction to respond to that. We need to observe Notifications to detect when the keyboard changes position. We must provide blocks to execute when URL Sessions respond with data. Finally, we use KVO to detect changes in variables.
 All of these various systems makes our code needlessly complex. Wouldn't it be better if there was one consistent system that handled all of our call/response code? Rx is such a system.
 
-### Basics
+### Concepts
 
 The key to understanding RxSwift is by understanding the notion of Observables as **sequences** of elements.
 The next step is to learn how to **create** them, **manipulate** them, and finally **subscribe** to them. Subscribing is needed in order to start the computation and the reception of the elements.
